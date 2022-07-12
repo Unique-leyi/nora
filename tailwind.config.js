6d5d730fe7922fbe3f9487b5e78cb2a95621e6b0
Ayoleyi Oluwatoki <71130409+Unique-leyi@users.noreply.github.com>
2022-07-12 11:02:23 -0800

    Made responsive changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         primary: '#0f1114',
