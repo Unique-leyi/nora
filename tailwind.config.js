1b7d6f8f16c98f2c16df2f7354bb7743b89a7fbd
Ayoleyi Oluwatoki <71130409+Unique-leyi@users.noreply.github.com>
2022-07-12 11:39:58 -0800

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"],
   theme: {
     screens: {
-      'xs': '480px',
+      'xs': '400px',
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
